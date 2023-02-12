de0c221101d172677c0128d025f09c1942d3d50e
felpiz230 <99624132+felpiz230@users.noreply.github.com>
2023-02-12 15:46:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+function Nome(){
+
+let arr = ["Munitor","Mouse","Teclado","CPU","MousePad"]
+console.log("lista de itens", arr)
+}
+Nome()
+
+let arr = ["Munitor","Mouse","Teclado","CPU","MousePad"]
+console.log(arr)
+arr.unshift("HeadPhone")
+
+arr.pop()
+console.log(arr)
+
+arr.push("cadeira","penDrive")
+console.log(arr)
+
+arr.shift()
+console.log(arr)
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+console.log(numbers)
+
+numbers.sort()
+console.log(numbers)
+
+let pedra = {
+    inteligente:'',
+    simpatico:'',
+    engraçado:''
+}
+console.log(pedra)
+
+pedra.calmo = '';
+
+delete pedra.inteligente
+
+pedra.inteligente = '';
+console.log(pedra)
+
+let cadastro = [{
+   nome: "",
+   idade: "",
+   telefone: "",
+   amigo: ["João","Pedro","Daiane","Leia"]
+},
+{
+  nome: "",
+  idade: "",
+  telefone: "",
+  amigo: ["Julia","Manuela","Gabriel","Lucas"]
+},
+{
+  nome: "",
+  idade: "",
+  telefone: "",
+  amigo: ["Camila","Guilherme","Maria","Sara"]
+},
+{
+  nome: "",
+  idade: "",
+  telefone: "",
+  amigo: ["Filipe","José","Gabriela","Gustavo"]
+},
+{
+  nome: "",
+  idade: "",
+  telefone: "",
+  amigo: ["Paulo","Roberto","Ana","Bianca"]
+}]
+
+
+
